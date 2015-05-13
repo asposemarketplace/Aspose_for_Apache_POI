eb1df743c27c6929b119e0adf9141ac609f1eb6d
Shoaib Khan <shoaib.khan@aspose.com>
2015-05-13 12:04:01 +0500

    New Examples for Missing Features of Apache POI HSLF-XSLF v1.4
    New Examples for Missing Features of Apache POI HSLF-XSLF v1.4

--- More Features of Aspose.Slides/src/asposefeatures/workingwithcharts/numberformatforchartdatacell/java/NumberFormatForChartDataCell.java
+++ More Features of Aspose.Slides/src/asposefeatures/workingwithcharts/numberformatforchartdatacell/java/NumberFormatForChartDataCell.java
@@ -14,6 +14,7 @@ public class NumberFormatForChartDataCell
     public static void main(String[] args)
     {
 	String dataPath = "src/asposefeatures/workingwithcharts/numberformatforchartdatacell/data/";
+	
 	// Instantiate the presentation//Instantiate the presentation
 	Presentation pres = new Presentation();
 
--- More Features of Aspose.Slides/src/asposefeatures/workingwithsmartart/checkingassistantnodes/java/CheckAssistantNodesInSmartArtShapes.java
+++ More Features of Aspose.Slides/src/asposefeatures/workingwithsmartart/checkingassistantnodes/java/CheckAssistantNodesInSmartArtShapes.java
@@ -12,42 +12,43 @@ public class CheckAssistantNodesInSmartArtShapes
     public static void main(String[] args)
     {
 	String dataPath = "src/asposefeatures/workingwithsmartart/checkingassistantnodes/data/";
-	
+
 	// Creating a presentation instance
-	   Presentation pres = new Presentation(dataPath + "presentation.pptx");
+	Presentation pres = new Presentation(dataPath + "presentation.pptx");
 
-	   // Traverse through every shape inside first slide
-	   for(IShape shape : pres.getSlides().get_Item(0).getShapes())
-	   {
-	       // Check if shape is of SmartArt type
-	       if (shape instanceof ISmartArt)
-	       {
-	           // Typecast shape to SmartArtEx
-	           ISmartArt smart = (SmartArt)shape;
+	// Traverse through every shape inside first slide
+	for (IShape shape : pres.getSlides().get_Item(0).getShapes())
+	{
+	    // Check if shape is of SmartArt type
+	    if (shape instanceof ISmartArt)
+	    {
+		// Typecast shape to SmartArtEx
+		ISmartArt smart = (SmartArt) shape;
 
-	           // Traversing through all nodes of SmartArt shape
-	           for(int i = 0; i < smart.getAllNodes().size(); i++)
-	           {
-	               ISmartArtNode node  =    smart.getAllNodes().get_Item(i);
+		// Traversing through all nodes of SmartArt shape
+		for (int i = 0; i < smart.getAllNodes().size(); i++)
+		{
+		    ISmartArtNode node = smart.getAllNodes().get_Item(i);
 
-	               String tc = node.getTextFrame().getText();
+		    String tc = node.getTextFrame().getText();
 
-	               // Check if node is Assistant node
-	               if (node.isAssistant())
-	               {
-	        	   System.out.println(tc + " - true");
-	                   // Setting Assistant node to false and making it normal node
-	                   node.setAssistant(false);
-	               }
-	               else
-	               {
-	        	   System.out.println(tc + " - false");	        	   
-	               }
-	           }
-	       }
-	   }
-	   //Save Presentation
-	   pres.save(dataPath + "AsposeChangeAssitantNode.pptx", SaveFormat.Pptx);
-	   System.out.println("AsposeChangeAssitantNode Saved.");
+		    // Check if node is Assistant node
+		    if (node.isAssistant())
+		    {
+			System.out.println(tc + " - true");
+			// Setting Assistant node to false and making it normal
+			// node
+			node.setAssistant(false);
+		    }
+		    else
+		    {
+			System.out.println(tc + " - false");
+		    }
+		}
+	    }
+	}
+	// Save Presentation
+	pres.save(dataPath + "AsposeChangeAssitantNode.pptx", SaveFormat.Pptx);
+	System.out.println("AsposeChangeAssitantNode Saved.");
     }
 }
